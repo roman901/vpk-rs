0ff3d81fa56adae003ba8d7f2622a4f7ff7df0b5
Roman Shishkin <spark@uwtech.org>
2018-07-23 16:40:28 +0300

    VPK header reading

--- examples/list.rs
+++ examples/list.rs
@@ -3,7 +3,16 @@ extern crate vpk;
 use std::env;
 
 fn main() {
-    println!("Hello world");
     let args: Vec<_> = env::args().collect();
-    println!("{}", args[1]);
+
+    if args.len() == 1 {
+        panic!("Input file is not specified");
+    }
+
+    let vpk_file = match vpk::open(&args[1]) {
+        Err(e) => panic!("Error while open file {}, err {}", &args[1], e),
+        Ok(vpk_file) => vpk_file
+    };
+
+    println!("{:#?}", vpk_file);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,47 @@
 use std::path::Path;
 use std::fs::File;
+use std::mem;
+use std::slice;
+use std::io::{Read, Error};
+use std::io::BufReader;
 
-struct VPKHeader {
+const VPK_SIGNATURE: u32 = 0x55aa1234;
+
+#[derive(Debug)]
+pub struct VPKHeader {
     signature: u32,
-    version: u32
+    version: u32,
+
+    tree_size: u32,
+    file_data_section_size: u32,
+    archive_md5_section_size: u32,
+    other_md5_section_size: u32,
+    signature_section_size: u32
 }
 
-struct VPKFile {
-
+#[derive(Debug)]
+pub struct VPKBundle {
+    header: VPKHeader
 }
 
-fn open(vpk_file: &str) -> VPKFile {
-    let path = Path::new(vpk_file);
-    let file = File::open(path);
+pub fn open(vpk_file: &String) -> Result<VPKBundle, Error> {
+    let p = Path::new(vpk_file);
+    let f = File::open(&p)?;
 
-    VPKFile {}
+    // Read header of file
+    let mut header: VPKHeader = unsafe { mem::uninitialized() };
+    let mut reader = BufReader::new(f);
+
+    unsafe {
+        let dst_ptr = &mut header as *mut VPKHeader as *mut u8;
+        let mut slice = slice::from_raw_parts_mut(dst_ptr, mem::size_of::<VPKHeader>());
+
+        reader.read_exact(slice);
+    }
+
+    if header.signature != VPK_SIGNATURE {
+        panic!("Specified file is not vpk dir file!");
+    }
+
+    Ok(VPKBundle {header})
 }
